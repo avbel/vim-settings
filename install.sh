4a6a6442324402c5b404407c0277f8384ba2d5a0
Andrey Belchikov <belchikovs@gmail.com>
2015-10-28 10:20:53 +0300

    Some fixes

--- install.sh
+++ install.sh
@@ -8,20 +8,20 @@ command -v ctags >/dev/null 2>&1 || { echo "ctags should be installed.  Aborting
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/plugin
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-wget –quiet "$BASE_URL/.vimrc" -O ~/.vimrc
+wget "$BASE_URL/.vimrc" -O ~/.vimrc -q
 if command -v tmux >/dev/null 2>&1; then
   wget "$BASE_URL/tmux.vim" -O ~/.vim/plugin/tmux.vim
 fi
 
 echo "Installing vim plugins ..."
-vi +NeoBundleInstall +xa >/dev/null 2>/dev/null
+sh -c "vi +NeoBundleInstall +x" >/dev/null 2>/dev/null
 
 echo "Changing plugins settings..."
-wget –quiet "$BASE_URL/current-theme.vim" -O ~/.vim/plugin/current-theme.vim
-wget –quiet "$BASE_URL/promptline.vim" -O ~/.vim/plugin/promptline.vim
+wget "$BASE_URL/current-theme.vim" -O ~/.vim/plugin/current-theme.vim -q
+wget "$BASE_URL/promptline.vim" -O ~/.vim/plugin/promptline.vim -q
 
 echo "Configuring shell..."
-vi +"PromptlineSnapshot ~/.shell_prompt.sh airline" +x >/dev/null 2>/dev/null
+sh -c 'vi +"PromptlineSnapshot ~/.shell_prompt.sh airline" +x' >/dev/null 2>/dev/null
 echo ". $HOME/.shell_prompt.sh" >> ~/.profile
 
 echo "Done"

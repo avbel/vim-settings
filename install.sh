7e7767322b185724d96a90ddaefff2540c144ca7
Andrey Belchikov <belchikovs@gmail.com>
2015-11-19 15:39:04 +0300

    Fixed issues with vim calling

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 BASE_URL=https://raw.githubusercontent.com/avbel/vim-settings/master
 
-command -v vi >/dev/null 2>&1 || { echo "VIM should be installed.  Aborting." >&2; exit 1; }
+command -v vim >/dev/null 2>&1 || { echo "VIM should be installed.  Aborting." >&2; exit 1; }
 
 [ -f ~/.vimrc ] && exit 0
 
@@ -10,7 +10,9 @@ git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim -q
 wget "$BASE_URL/.vimrc" -O ~/.vimrc -q
 
 echo "Installing vim plugins ..."
-vi +NeoBundleInstall +x >/dev/null 2>/dev/null || true
+
+
+vim +NeoBundleInstall +x </dev/tty >/dev/null 2>/dev/null
 
 echo "Changing plugins settings..."
 wget "$BASE_URL/current-theme.vim" -O ~/.vim/plugin/current-theme.vim -q
@@ -19,5 +21,6 @@ wget "$BASE_URL/shell.vim" -O ~/.vim/plugin/shell.vim -q
 [ -f ~/.shell_prompt.sh ] && exit 0
 
 echo "Configuring shell..."
-vi +"PromptlineSnapshot ~/.shell_prompt.sh airline" +x >/dev/null 2>/dev/null || true
+
+vim +"PromptlineSnapshot ~/.shell_prompt.sh airline" +x </dev/tty >/dev/null 2>/dev/null
 echo ". $HOME/.shell_prompt.sh" >> ~/.profile
--- node-install.sh
+++ node-install.sh
@@ -1,13 +1,13 @@
 BASE_URL=https://raw.githubusercontent.com/avbel/vim-settings/master
 
 command -v npm >/dev/null 2>&1 || { echo "npm should be installed.  Aborting." >&2; exit 1; }
-command -v vi >/dev/null 2>&1 || { echo "vim should be installed.  Aborting." >&2; exit 1; }
+command -v vim >/dev/null 2>&1 || { echo "vim should be installed.  Aborting." >&2; exit 1; }
 
 [ -d ~/.vim/plugin ] || mkdir -p ~/.vim/plugin
 
 echo "Installing vim plugins for NodeJS ..."
 wget "$BASE_URL/node.vim" -O ~/.vim/plugin/node.vim -q
-(vi +NeoBundleInstall +x >/dev/null 2>/dev/null)
+vim +NeoBundleInstall +x </dev/tty >/dev/null 2>/dev/null
 echo "Installing some required tools for NodeJS"
 npm install -g eslint eslint_d >/dev/null 2>/dev/null
 (cd ~/.vim/bundle/tern_for_vim && npm install >/dev/null 2>/dev/null)
